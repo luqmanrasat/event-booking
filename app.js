cd38eb4ddec7dd2d897954682f91af5a67f65dd2
Luqman <luqmanrasat@hotmail.com>
2019-07-22 02:10:55 +0800

    wip

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello Luqman');
-});
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {}
+}));
 
 app.listen(3000);
